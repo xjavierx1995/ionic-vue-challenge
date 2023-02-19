ddb0683620ed780a2434bdefaabeac63298ccfc2
Javier Rodriguez <xjavierx1995@gmail.com>
2023-02-19 03:25:35 -0400

    card design

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,12 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@500&family=Roboto&display=swap');
+	ion-content{
+		--background: #dcdde8;
+		--padding-end: 16px;
+    --padding-start: 16px;
+		font-family: 'Roboto' !important;
+	}
+</style>
--- src/components/pokemonCard.vue
+++ src/components/pokemonCard.vue
@@ -1,21 +1,26 @@
 <template>
 	<ion-card>
-		<img 
-		style="height: 159px;"
+		<div class="image-container">
+			<img 
+			style="height: 159px;"
 			:src="pokemon.detail.sprites.other.home.front_default ?? pokemon.detail.sprites.other['official-artwork'].front_default" 
-		/>
+			/>
+		</div>
 		<ion-card-header>
 			<ion-card-title>{{ pokemon.name }}</ion-card-title>
 			<ion-card-subtitle>#{{ pokemon.detail.order }}</ion-card-subtitle>
 		</ion-card-header>
 
 		<ion-card-content>
-			Moves <strong>{{ pokemon.detail.moves.length }}</strong> Experience <strong>{{ pokemon.detail.base_experience }} </strong>
+			Moves <span class="value">{{ pokemon.detail.moves.length }}</span> Experience <span class="value">{{ pokemon.detail.base_experience }} </span>
 		</ion-card-content>
 
-		<ion-button v-for="(type, index) in pokemon.detail.types" :key="index" fill="clear" >
+		<div class="type-container">
+			<ion-chip v-for="(type, index) in pokemon.detail.types" :key="index" color="medium">{{ type.type.name }}</ion-chip>
+		</div>
+		<!-- <ion-button v-for="(type, index) in pokemon.detail.types" :key="index" fill="clear" >
 			<ion-chip color="medium">{{ type.type.name }}</ion-chip>
-		</ion-button>
+		</ion-button> -->
 	</ion-card>
 </template>
 <script setup lang="ts">
@@ -29,7 +34,66 @@
 
 	const { pokemon } = toRefs(props);
 </script>
-<style lang="">
+<style scoped lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@500&display=swap');
 	
+	ion-card{
+		--background: #fdfdfd;
+		margin: 24px 0;
+		border-radius: 16px;
+		box-shadow: none !important;
+		ion-card-title{
+			font-family: 'Readex Pro';
+			font-weight: 500;
+			font-size: 20px;
+			line-height: 28px;
+		}
+		ion-card-subtitle{
+			font-family: 'Roboto';
+			font-style: normal;
+			font-weight: 400;
+			font-size: 14px;
+			line-height: 22px;
+			color: #8D91A5;
+		}
+
+		ion-card-content{
+			color: #8D91A5;
+			gap: 7px;
+			padding-bottom: 0;
+    	display: flex;
+		}
+	}
+
+	.type-container{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: 0px;
+		gap: 8px;
+		margin: 16px 16px;
+		width: 296px;
+		height: 32px;
+
+	}
+
+	.image-container{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		background: #ececf2;
+	}
 	
+	.value{
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 14px;
+		line-height: 22px;
+		/* identical to box height, or 157% */
+
+
+		color: #55596D;
+	}
 </style>
